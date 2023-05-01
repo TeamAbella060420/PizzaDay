9a3a597e8d22df2adbe3aad2ac907ffba4a7b23e
TeamAbella060420 <teamabella@outlook.com>
2023-05-01 20:32:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -66,3 +66,18 @@ allSections.forEach((section) => {
   sectionObserver.observe(section);
   section.classList.add('section__hidden');
 });
+
+// mobile menu 
+
+const mobileMenu = document.querySelector('.mobile__menu');
+const overlay = document.querySelector('.navigation')
+
+const showMenu = () => {
+  overlay.classList.add('show__menu')
+}
+const hideMenu = () => {
+  overlay.classList.remove('show__menu')
+}
+
+mobileMenu.addEventListener('click', showMenu)
+overlay.addEventListener('click', hideMenu)
